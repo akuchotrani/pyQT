4fffc6b91a4ffa68898b72ce6479edbf523fab9c
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-12-01 17:38:15 -0800

    adding file menu and options

--- BasicWindow.py
+++ BasicWindow.py
@@ -6,14 +6,63 @@ Created on Thu Nov 30 15:23:07 2017
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication,QWidget, QMainWindow, QPushButton, QAction
 
-app = QApplication(sys.argv)
 
 
-window = QWidget()
-window.setGeometry(50,50,500,300)
-window.setWindowTitle("Course Recommender")
+class window(QMainWindow):
+    
+    def __init__(self):
+        super(window,self).__init__()
+        self.setGeometry(50,50,500,300)
+        self.setWindowTitle('DigiPen Course Recommender')
+        self.setWindowIcon(QIcon(r'C:\Users\aakash.chotrani\Desktop\pyQT\DigiPenLogo_new.png'))
+        
+        
+        extractAction = QAction('&Quit Application',self)
+        extractAction.setShortcut('ctrl+s')
+        extractAction.setStatusTip('Leave the app')
+        extractAction.triggered.connect(self.close_application)
+        
+        self.statusBar()
+        
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&File')
+        fileMenu.addAction(extractAction)
+        
+        
+        
+        
+        
+        
+        self.home()
+        
+        
+        
+        
 
-window.show()
-sys.exit(app.exec_())
+    def home(self):
+        btn = QPushButton('quit', self)
+        btn.resize(100, 100)
+        btn.move(100, 100)
+        #btn.clicked.connect(QCoreApplication.instance().quit)
+        btn.clicked.connect(self.close_application)
+        self.show()
+        
+    def close_application(self):
+        print("Hello the application is closed")
+        sys.exit()
+        
+def run():
+    app = QApplication(sys.argv)
+    Gui = window()
+    sys.exit(app.exec_())
+
+
+run()    
+        
+
+
